c8723c53a30d4a4714380f67b4a021bac36ecddb
Gabriel de Campos <gabodecampos@gmail.com>
2022-09-04 18:20:27 -0400

    Criação no layout global e paginas

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-app-bar
+        app
+        color="shape_dark"
+        dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://www.citypng.com/public/uploads/preview/-51608495204iyvpoqgita.png"
+            transition="scale-transition"
+            width="40"
+        />
+
+        <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main class="bg">
+      <router-view />
+    </v-main>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MyLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
