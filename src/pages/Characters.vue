aba0691a448f09f0f61b703b18e2bb63734e0948
Gabriel de Campos <gabodecampos@gmail.com>
2022-09-06 21:41:16 -0400

    Paginação

--- src/pages/Characters.vue
+++ src/pages/Characters.vue
@@ -27,41 +27,69 @@
         </v-text-field>
       </v-col>
 
-      <characters-card
-        v-for="(character, index) in listCharacters"
-        :key="index + 'character'"
-        :character="character"
-      />
+      <v-row>
+        <characters-card
+            v-for="(character, index) in listCharacters"
+            :key="index + 'character'"
+            :character="character"
+
+        />
+      </v-row>
+
+      <v-col cols="12">
+        <div class="text-center">
+          <v-pagination
+              v-model="pagination.page"
+              :length="pagination.count"
+              :total-visible="4"
+              color="#FFE81F"
+              dark
+              @previous="getCharacters(pagination.prevPage)"
+              @next="getCharacters(pagination.nextPage)"
+          ></v-pagination>
+        </div>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 import CharactersCard from "@/components/CharactersCard";
 export default {
   name: 'HomePage',
-  components: {CharactersCard},
+  components: { CharactersCard },
   data: () => ({
     listCharacters: [],
-    nextPage: '',
-    prevPage: '',
+
     searchCharacter: '',
     mesage: '',
-    debouncedGetAnswer: ''
+    debouncedGetAnswer: '',
+    pagination:{
+      page: 1,
+      count: 0,
+      nextPage: '',
+      prevPage: '',
+    }
   }),
 
+  watch: {
 
+  },
   mounted() {
-    this.getCharacters()
+    this.getCharacters('https://swapi.dev/api/people')
   },
 
   methods: {
     async getCharacters(search) {
-      let uri = search ? `/people&name=${search}` : '/people'
-
-      await this.http.get(`${uri}`)
+      await axios.get(search)
           .then((r) => {
             this.listCharacters = r.data.results
+            this.pagination.count = r.data.count
+            this.pagination.nextPage = r.data.next
+            this.pagination.prevPage = r.data.previous
+
             return console.log('listCharacters=> ',this.listCharacters)
           }).catch((e) => {
             console.error(e)
@@ -82,15 +110,15 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  padding-right: 5vw;
+  padding-left: 5vw;
 }
 
 .characters__title {
   font-family: 'StarJedi';
   font-weight: bold;
   color: $title;
-  padding-right: 2rem;
   text-align: center;
-  letter-spacing: 1.6px;
   opacity: 1;
 }
 
@@ -99,6 +127,8 @@ export default {
   max-width: 570px;
   border: solid 2px #606060 !important;
   color: $text;
+  align-content: center;
+  align-items: center;
 
   .characters__search-icon{
     color: $text;
